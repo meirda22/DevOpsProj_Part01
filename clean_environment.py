b649cc9e27f1c4e2c15eb7d56bf19e57b9410916
meirda22 <meirda22@gmail.com>
2023-01-12 00:45:22 +0200

    DevOpsProject2023

--- clean_environment.py
+++ clean_environment.py
@@ -3,14 +3,14 @@ import requests
 
 time.sleep(10)
 res1 = requests.get(f'http://127.0.0.1:5000/stop_server')
-if res1 is not False:
-    print('Server Stopped')
+if res1.status_code:
+    print('Server Stopped, Code: ' + str(res1.status_code))
 else:
     raise Exception('Could not stop server, Try again later...')
 
 res2 = requests.get(f'http://127.0.0.1:5001/stop_server')
-if res2 is not False:
-    print('Server Stopped')
+if res2.status_code:
+    print('Server Stopped, Code: ' + str(res2.status_code))
 else:
     raise Exception('Could not stop server, Try again later...')
 
