cd1a3daa5b4c0376027c221d4f7361b943a8329e
meirda22 <meirda22@gmail.com>
2023-01-11 22:59:20 +0200

    DevOpsProject2023

--- web_app.py
+++ web_app.py
@@ -1,5 +1,8 @@
+import signal
+
 from flask import Flask
 from db_connector import get_user_name_from_db
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +16,11 @@ def get_user_name(user_id):
         return "<h1 id='error'>" + "No User Found for ID : " + user_id + "</h1>"
     else:
         return "<h1 id='userName'>" + "User Name For User Id " + user_id + " is : " + user_name + "</h1>"
+
+@app.route('/stop_server')
+def stop_server():
+    os.kill(os.getpid(), signal.CTRL_C_EVENT)
+    return "Server stopped"
 
 
 app.run(host='127.0.0.1', debug=True, port=5001)
